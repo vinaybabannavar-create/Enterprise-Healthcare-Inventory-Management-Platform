82e846d531f986ca1692b21c684fac02bad2adb1
vinaybabannavar <vinaybabannavar@gmail.com>
2026-02-01 23:18:02 +0530

    Chore: Configure backend for ASGI deployment on Render

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-mk9l8k#%2husar6_tu3!x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,*.onrender.com').split(',')
 
 
 # Application definition
